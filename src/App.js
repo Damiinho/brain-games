b1d90b5f912969e56acb1cc4b9aa131ce8cc6547
Damian Ludef <damiinho@gmail.com>
2023-11-28 00:13:08 +0100

    +error + 2 buttons

--- src/App.js
+++ src/App.js
@@ -53,6 +53,100 @@ function App() {
             </div>
           </Button>
         </Link>
+        <Link to={`division`}>
+          <Button
+            size="small"
+            variant="contained"
+            style={{
+              backgroundColor: "rgb(63, 130, 185, 0.7)",
+            }}
+          >
+            <div style={buttonStyle}>
+              <span style={spanStyle}>division</span>
+              <div
+                style={{
+                  width: 35,
+                  height: 35,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <div
+                  style={{
+                    width: 26,
+                    height: 26,
+                    backgroundColor: "#fb00aa",
+                    borderRadius: 3,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <span
+                    style={{
+                      color: "#376085",
+                      fontWeight: "bold",
+                      fontSize: 40,
+                      fontFamily: "Quicksand, sans-serif",
+                      transform: "rotate(-45deg)",
+                      position: "relative",
+                      top: -2,
+                      left: -2,
+                    }}
+                  >
+                    –
+                  </span>
+                </div>
+              </div>
+            </div>
+          </Button>
+        </Link>
+        <Link to={`multiplication`}>
+          <Button
+            size="small"
+            variant="contained"
+            style={{
+              backgroundColor: "rgb(63, 130, 185, 0.7)",
+            }}
+          >
+            <div style={buttonStyle}>
+              <span style={spanStyle}>multiplication</span>
+              <div
+                style={{
+                  width: 35,
+                  height: 35,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <div
+                  style={{
+                    width: 26,
+                    height: 26,
+                    backgroundColor: "#fb00aa",
+                    borderRadius: 3,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <span
+                    style={{
+                      color: "#376085",
+                      fontWeight: "bold",
+                      fontSize: 30,
+                      fontFamily: "Quicksand, sans-serif",
+                    }}
+                  >
+                    ×
+                  </span>
+                </div>
+              </div>
+            </div>
+          </Button>
+        </Link>
       </div>
     </div>
   );
--- src/error-page.js
+++ src/error-page.js
@@ -1,16 +1,30 @@
-import { useRouteError } from "react-router-dom";
+import { Button } from "@mui/material";
+import { Link, useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+    <div
+      id="error-page"
+      style={{
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "50px",
+      }}
+    >
+      <h1>Work in progress</h1>
+      {/* <p>
         <i>{error.statusText || error.message}</i>
-      </p>
+      </p> */}
+      <Link to="/">
+        <Button variant="contained">back to home page</Button>
+      </Link>
     </div>
   );
 }
